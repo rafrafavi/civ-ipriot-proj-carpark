50d95712e7cc23bd13b1e7105e8998c8a00405d8
SAE INOUE <INOUES@tdm.local>
2023-05-31 12:34:05 +0800

    try to fix some error

--- smartpark/simple_mqtt_carpark.py
+++ smartpark/simple_mqtt_carpark.py
@@ -2,13 +2,12 @@ import paho.mqtt.client as paho
 import simple_mqtt_device
 class Carpark(simple_mqtt_device.MqttDevice):
     """Creats a carpark object to store the state of cars in the lot"""
-
     def __init__(self,config):
         super().__init__(config)
         self.total_spaces = config['total-spaces']
         self.total_cars = config['total-cars']
         self.client.on_message = self.on_message
-        self.client.subscribe(self.topic)
+        self.client.subscribe('+/+/+/+')
         self.client.loop_forever()
 
 
@@ -20,24 +19,24 @@ class Carpark(simple_mqtt_device.MqttDevice):
         # TODO: Publish to MQTT
     def car_exit(self):
         self.total_cars -= 1
-        #TODO: Publish to MQTT
+        # TODO: Publish to MQTT
 
     def on_message(self, client, userdata, msg):
         print(f'Received {msg.payload.decode()}')
 
-    if __name__ == '__main__':
-        config = {'name': 'saepark',
-                  'total-spaces': 130,
-                  'total-cars': 0,
-                  'location': 'L306',
-                  'topic': "lot/sensor",
-                  'broker': 'localhost',
-                  'port': 1883,
-                  'type': 'ENTRY'
-                  }
+if __name__ == '__main__':
+    config = {'name': 'saepark',
+              'total-spaces': 130,
+              'total-cars': 0,
+              'location': 'L306',
+              'topic': "lot/sensor",
+              'broker': 'localhost',
+              'port': 1883,
+              'type': 'ENTRY'
+              }
 
-        carpark = Carpark(config)
-        print("Carpark initialized")
+carpark = Carpark(config)
+print("Carpark initialized")
 
 
 
--- smartpark/simple_mqtt_device.py
+++ smartpark/simple_mqtt_device.py
@@ -1,35 +1,25 @@
+import paho.mqtt.client as paho
 class MqttDevice:
-    def __init__(self, config):
+    def __init__(self, config, device_name='sensor'):
         self.name = config['name']
         self.location = config['location']
-        self.topic = config['topic']
+
+        # Define topic components:
+        self.topic_root = config['topic-root']
+        self.topic_qualifier = config['topic-qualifier']
+        self.topic_device = device_name
+        self.topic = self._create_topic_string()
+
+        # Configure broker
         self.broker = config['broker']
         self.port = config['port']
+
+        # initialise a paho client and bind it to the object (has-a)
+
         self.client = paho.Client()
         self.client.connect(self.broker,
                             self.port)
-        self.type = config['type']  #entry and exit
 
-        #initialise a paho client and bind it to the object(has-a)
-
-        client = paho.Client()
-        client.connect(self.broker,
-                       self.port)
-        client.publish("lot/sensor", "Car Detected")
-
-
-
-    def on_detection(self, message):
-        """The method that is triggered when a detection occurs"""
-        message = f'{self.type}, {message}'
-        print(message)
-        self.client.publish(self.topic, message)
-
-    def start_sensing(self):
-        """a blocking event loop that waits for detection events, in this case"""
-        while True:
-            reply = input("Is there a car?")
-            if reply == 'y':
-                self.on_detection("Car, I saw a car!!")
-            elif reply == 'n':
-                self.on_detection("Car, I saw a car!!")
+    def _create_topic_string(self):
+        return (f"{self.topic_root}/{self.location}/" +
+                f"{self.topic_device}/{self.topic_qualifier}")
