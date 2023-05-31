9dbc7d242be47f1ced7f8083c3812bd1f2041038
SAE INOUE <INOUES@tdm.local>
2023-05-31 10:49:53 +0800

    update senser

--- smartpark/Sensor.py
+++
@@ -1,9 +0,0 @@
-
-from sense_hat import SenseHat
-
-class Sensor:
-    def __init__(self):
-        self.sense_hat = SenseHat()
-
-    def get_temperature(self):
-        return self.sense_hat.get_temperature()
