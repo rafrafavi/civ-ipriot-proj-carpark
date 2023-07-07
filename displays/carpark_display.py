7180ba2992f7057edc8a4b81191e607296870249
SicrosoftMurface <20104196@tafe.wa.edu.au>
2023-07-07 21:01:21 +0800

    Seperate no_pi.py into classes

---
+++ displays/carpark_display.py
@@ -0,0 +1,25 @@
+class CarParkDisplay:
+    """Provides a simple display of the car park status. This is a skeleton only. The class is designed to be customizable without requiring and understanding of tkinter or threading."""
+    # determines what fields appear in the UI
+    fields = ['Available bays', 'Temperature', 'At']
+
+    def __init__(self):
+        self.window = WindowedDisplay(
+            'Moondalup', CarParkDisplay.fields)
+        updater = threading.Thread(target=self.check_updates)
+        updater.daemon = True
+        updater.start()
+        self.window.show()
+
+    def check_updates(self):
+        # TODO: This is where you should manage the MQTT subscription
+        while True:
+            # NOTE: Dictionary keys *must* be the same as the class fields
+            field_values = dict(zip(CarParkDisplay.fields, [
+                f'{random.randint(0, 150):03d}',
+                f'{random.randint(0, 45):02d}℃',
+                time.strftime("%H:%M:%S")]))
+            # Pretending to wait on updates from MQTT
+            time.sleep(random.randint(1, 10))
+            # When you get an update, refresh the display.
+            self.window.update(field_values)
